0b39ddf2b5963ea733e1a52201fd72a8acf56b6d
JossZC <jocselinz@hotmail.com>
2016-02-01 19:09:46 -0600

    Version 1

---
+++ HelloWorld/Estudiante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Estudiante
+    {
+
+        // Field
+        public string name;
+
+        // Constructor thattakesno arguments.
+        public Estudiante()
+        {
+            name = "unknown";
+        }
+
+        // Constructor thattakesoneargument.
+        public Estudiante(string nm)
+        {
+            name = nm;
+        }
+        // Method
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+    }
+}
